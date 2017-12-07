6ae577f9977e8e4e4a6a4601fbc1faf4afb40ee7
oussou-dev <oussou.dev@gmail.com>
2017-12-07 14:56:24 +0100

    Implémenter le scan

--- src/pages/scan/scan.ts
+++ src/pages/scan/scan.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import {BarcodeScanner, BarcodeScannerOptions, BarcodeScanResult} from '@ionic-native/barcode-scanner'
+import { ToastController } from 'ionic-angular/components/toast/toast-controller';
+
 
 @IonicPage()
 @Component({
@@ -10,9 +12,12 @@ import {BarcodeScanner, BarcodeScannerOptions, BarcodeScanResult} from '@ionic-n
 })
 export class ScanPage {
 
+  result: BarcodeScanResult
+
   constructor(public navCtrl: NavController, 
               public navParams: NavParams,
-              private bcs: BarcodeScanner) {
+              private bcs: BarcodeScanner,
+              private toastCtrl: ToastController) {
   }
 
   ionViewDidLoad() {
@@ -20,7 +25,21 @@ export class ScanPage {
   }
 
   scanBarcode() {
-    
+    const options: BarcodeScannerOptions = {
+      prompt: 'Pointer votre camera vers un code barre', // message à afficher
+      torchOn: false, // la lampe du phone ne s'allume pas
+    }
+
+    // implémentation du scan
+    this.bcs.scan(options)
+        .then(res => {
+          this.result = res;
+        })
+      .catch(err => {
+        this.toastCtrl.create({
+          message: err.message
+        }).present()
+      })
   }
 
 }
