5b63f23e0516472864c95f0601f48accac6cb20c
oussou-dev <oussou.dev@gmail.com>
2017-12-07 16:33:55 +0100

    Requêter une API REST [openfoodfacts]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
+import {HttpClientModule} from '@angular/common/http'
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
-// import { ScanPage } from './../pages/scan/scan';
+import { ScanPage } from './../pages/scan/scan';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -20,6 +22,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     ScanPageModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
--- src/pages/scan/scan.ts
+++ src/pages/scan/scan.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 
 import {BarcodeScanner, BarcodeScannerOptions, BarcodeScanResult} from '@ionic-native/barcode-scanner'
+import { HttpClient } from '@angular/common/http';
 // import { ToastController } from 'ionic-angular/components/toast/toast-controller';
 
 
@@ -14,10 +15,17 @@ export class ScanPage {
 
   result: BarcodeScanResult
 
+  BASE_URL = 'https://world.openfoodfacts.org/api/v0/product/'
+ 
+  // variables à utiliser côté template
+  api_response_raw;
+  api_response;
+
   constructor(public navCtrl: NavController, 
               public navParams: NavParams,
               private bcs: BarcodeScanner,
-              private toastCtrl: ToastController) {
+              private toastCtrl: ToastController,
+              private _http: HttpClient) {
   }
 
   ionViewDidLoad() {
@@ -64,4 +72,19 @@ export class ScanPage {
 //   }
 
 
+  getArticleByBarcode(code) {
+    this._http
+          .get(`${this.BASE_URL}${code}`)
+          .subscribe(data => this.displayResult(data), error => this.handleGetError(error))
+  }
+
+  displayResult(data) {
+    this.api_response_raw = data;
+    this.api_response = data
+  }
+
+  handleGetError(error) {
+    console.log(error);
+    console.error(error.message);
+  }
 }
